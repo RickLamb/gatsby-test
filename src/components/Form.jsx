50f6fe2881e08a652b821a9028acfd1bb1772880
RickLamb <rick_lamb@hotmail.com>
2020-08-17 19:11:25 -0700

    trying random changes to make form work

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -6,15 +6,11 @@ import "./Form.scss";
 const Form = () => {
   return (
     <div className='backend'>
-      <form name="register" method="POST" netlify-honeypot="bot-field" data-netlify="true" data-netlify-recaptcha="true" >
+      <form name="register" method="POST" data-netlify="true"  >
         <input type="hidden" name="form-name" value="register" />
-        <p className="hidden">
-          <input name="bot-field" id="bot-field" />
-        </p>
         <p>
           <input type="text" name="email" id="email" />
         </p>
-        <div data-netlify-recaptcha="true" />
         <p>
           <button type="submit">Send</button>
         </p>
